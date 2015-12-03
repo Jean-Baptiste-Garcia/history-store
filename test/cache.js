f9fee1b95c602015bdabaae160ecae2613d3db78
Jean-Baptiste-Garcia <garcia.jean-baptiste@orange.fr>
2015-12-03 18:45:39 +0100

    add 2 stores test

--- test/cache.js
+++ test/cache.js
@@ -254,6 +254,106 @@ describe('fs cached history-trend', function () {
 
     });
 
+    describe('2 stores', function () {
+        var hsA,
+            cachefolderA = storageRoot + '/MyServerA/trends/',
+            cachefileA =  cachefolderA + 'anonymous.json',
+            hsB = historystore(storageRoot).report('MyServerB'),
+            cachefolderB = storageRoot + '/MyServerB/trends/',
+            cachefileB = cachefolderB + 'anonymous.json';
+
+
+        beforeEach(function startAndPopulateServer(done) {
+            fse.removeSync(path.resolve(storageRoot));
+            hsA = historystore(storageRoot).report('MyServerA');
+            hsB = historystore(storageRoot).report('MyServerB');
+
+            var reportsA = [
+                    { date: new Date('1995-12-17T03:24:00'), status: {sessionCount: 100, schemasCount: 10}},
+                    { date: new Date('1995-12-18T04:44:10'), status: {sessionCount: 101, schemasCount: 5}},
+                    { date: new Date('1995-12-19T05:44:10'), status: {sessionCount: 102, schemasCount: 20}}
+                ],
+                reportsB = [
+                    {date: new Date('1995-12-14T04:44:20'), status: {sessionCount: 401, schemasCount: 10}},
+                    {date: new Date('1995-12-15T05:44:20'), status: {sessionCount: 402, schemasCount: 5}}
+                ];
+            async.series(reportsA.map(function makePut(report) {
+                return function put(callback) {
+                    hsA.put(report, callback);
+                };
+            }), async.series(reportsB.map(function makePut(report) {
+                return function put(callback) {
+                    hsB.put(report, callback);
+                };
+            }), done));
+        });
+
+
+        it('dont crash when 2 stores : store1.put, store1.trends, storeB.put, storeB.trends', function (done) {
+            var qA = hsA.cache(H.timeserie('status.sessionCount')),
+                qB =  hsB.cache(H.timeserie('status.sessionCount')),
+                statA,
+                statB;
+            fse.ensureDirSync(cachefolderA);
+
+            fse.writeFileSync(cachefileA, JSON.stringify([
+                { date: new Date('1995-12-17T03:24:00'), sessionCount: 200},
+                { date: new Date('1995-12-18T04:44:10'), sessionCount: 201},
+                { date: new Date('1995-12-19T05:44:10'), sessionCount: 202}
+            ]));
+            statA = fse.statSync(cachefileA);
+
+            fse.ensureDirSync(cachefolderB);
+            fse.writeFileSync(cachefileB, JSON.stringify([
+                { date: new Date('1995-12-14T04:44:20'), sessionCount: 401},
+                { date: new Date('1995-12-15T05:44:20'), sessionCount: 402}
+            ]));
+            statB = fse.statSync(cachefileB);
+
+
+            hsA.put({date: new Date('1995-12-20T03:44:10'), status: {sessionCount: 110, schemasCount: 20}}, function (err) {
+                if (err) { return done(err); }
+                qA.trends(function (err, trendsA) {
+                    var stata;
+                    if (err) { return done(err); }
+                    fse.statSync(cachefileA).should.not.eql(statA);
+                    statA = fse.statSync(cachefileA);
+                    trendsA.should.eql([
+                        { date: new Date('1995-12-17T03:24:00'), sessionCount: 200},
+                        { date: new Date('1995-12-18T04:44:10'), sessionCount: 201},
+                        { date: new Date('1995-12-19T05:44:10'), sessionCount: 202},
+                        { date: new Date('1995-12-20T03:44:10'), sessionCount: 110}
+                    ]);
+
+                    hsB.put({date: new Date('1995-12-20T05:44:20'), status: {sessionCount: 403, schemasCount: 20}}, function (err) {
+                        if (err) {return done(err); }
+                        qB.trends(function (err, trendsB) {
+                            if (err) { return done(err); }
+                            trendsB.should.eql([
+                                { date: new Date('1995-12-14T04:44:20'), sessionCount: 401},
+                                { date: new Date('1995-12-15T05:44:20'), sessionCount: 402},
+                                { date: new Date('1995-12-20T05:44:20'), sessionCount: 403}
+                            ]);
+                            fse.statSync(cachefileB).should.not.eql(statB);
+
+                            qA.trends(function (err, trendsA) {
+                                if (err) { return done(err); }
+                                trendsA.should.eql([
+                                    {date: new Date('1995-12-17T03:24:00'), sessionCount: 200},
+                                    {date: new Date('1995-12-18T04:44:10'), sessionCount: 201},
+                                    {date: new Date('1995-12-19T05:44:10'), sessionCount: 202},
+                                    {date: new Date('1995-12-20T03:44:10'), sessionCount: 110}
+                                ]);
+                                fse.statSync(cachefileA).should.eql(statA);
+                                done();
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+
 
 
     describe('with named query', function () {
